32d65d9c8307f1bb31f5cdeed100f5e9b3d854fc
Claudio <claudiovf@hotmail.com>
2020-09-21 15:16:33 +1000

    uibuild

---
+++ build/precache-manifest.a269615b426671aec912b046283c966a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f465d15ee37ad87685a7084a5b4c586c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a04ea9bb2c6944117e32",
+    "url": "/static/css/main.7016b887.chunk.css"
+  },
+  {
+    "revision": "763c347e7879460bee4b",
+    "url": "/static/js/2.ea02c14d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ea02c14d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a04ea9bb2c6944117e32",
+    "url": "/static/js/main.0947b3f5.chunk.js"
+  },
+  {
+    "revision": "caf43f0a14314da69f12",
+    "url": "/static/js/runtime-main.226f7c93.js"
+  }
+]);
